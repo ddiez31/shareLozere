d9dce418d7dcd5ca78053779f3cece32775f92c5
ange <ange@localhost.localdomain>
2017-06-27 17:59:51 +0200

    ajout d'une photo pour la background

--- client.php
+++ client.php
@@ -1,22 +1,42 @@
 <?php
 
+?>
 
+<!DOCTYPE html>
 <html>
 
 <head>
   <meta charset="utf-8">
   <title>Demande de prestation</title>
+  <link rel="stylesheet" href="./style/client.css">
 </head>
 
 <body>
-  <h1>Vous cherchez un professionnel du web pour:</h1>
+
+<div class="conteneur">
+
+  <div class="choix">
+
+  <h1>Vous cherchez un professionnel:</h1>
   <ul>
-    <li><input type="radio" name="" value="webdesign">webdesign</li>
-    <li><input type="radio" name="" value="digital marketing">digital marketing</li>
+    <li><input type="radio" name="" value="photo">pour faire des photos</li>
+    <li><input type="radio" name="" value="digital marketing">en e-commerce</li>
+    <li><input type="radio" name="" value="marketing">en marketing</li>
+    <li><input type="radio" name="" value="referencement">du référencement</li>
+    <li><input type="radio" name="" value="formation">pour vous dispenser une formation</li>
+    <li><input type="radio" name="" value="conseil">pour vous conseiller</li>
+    <li><input type="radio" name="" value="design">pour le design de votre site internet</li>
+    <li><input type="radio" name="" value="site">pour créer un site internet</li>
+    <li><input type="radio" name="" value="referencement">pour créer une appli mobile</li>
   </ul>
+
+  <form class="validation" action="index.html" method="post">
+    <input type="button" name="" value="valider">
+
+  </form>
+</div>
+</div>
 
 </body>
 
 </html>
-
-?>
