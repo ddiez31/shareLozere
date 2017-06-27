0993acabc9e57f221fde20c1210d8f675b5b65a2
ange <ange@localhost.localdomain>
2017-06-27 17:07:31 +0200

    bascule de client.html en client.php

---
+++ client.php
@@ -0,0 +1,22 @@
+<?php
+
+
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <title>Demande de prestation</title>
+</head>
+
+<body>
+  <h1>Vous cherchez un professionnel du web pour:</h1>
+  <ul>
+    <li><input type="radio" name="" value="webdesign">webdesign</li>
+    <li><input type="radio" name="" value="digital marketing">digital marketing</li>
+  </ul>
+
+</body>
+
+</html>
+
+?>
