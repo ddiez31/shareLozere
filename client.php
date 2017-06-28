7da0cca888a8e0f38aba39a44ec14789afb13490
David <david.diez31@free.fr>
2017-06-28 09:24:28 +0200

    reset

--- client.php
+++ client.php
@@ -1,22 +1,30 @@
 <?php
 
 ?>
+
 <!DOCTYPE html>
 <html>
+
 <head>
   <meta charset="utf-8">
-  <title>Resultat de prestation</title>
-  <link rel="stylesheet" href="./style/resultat.css">
+  <title>Demande de prestation</title>
+  <link rel="stylesheet" href="./style/client.css">
 </head>
+
 <body>
+
 <header>
 <img src="./style/logoShareLozere.png" alt="logo">
 </header>
+
+
 <div class="conteneur">
+
   <div class="choix">
+
   <h1>Vous cherchez un professionnel:</h1>
   <ul>
-    <li><input type="checkbox" name="" value="photo">pour faire des photos</li>
+    <li><input type="checkbox" name="photo" value="photo">pour faire des photos</li>
     <li><input type="checkbox" name="" value="digital marketing">en e-commerce</li>
     <li><input type="checkbox" name="" value="marketing">en marketing</li>
     <li><input type="checkbox" name="" value="referencement">du référencement</li>
@@ -26,13 +34,22 @@
     <li><input type="checkbox" name="" value="site">pour créer un site internet</li>
     <li><input type="checkbox" name="" value="referencement">pour créer une appli mobile</li>
   </ul>
-  <form class="validation" action="index.html" method="post">
+
+  <!-- <form class="validation" action="index.html" method="post">
     <a href="#">Valider</a>
+
+  </form> -->
+
+  <form class="validation" action="traitementClient.php" method="post" onSubmit="return validation(this);" enctype="multipart/form-data" ><a href="traitementClient.php">Valider</a>
   </form>
 </div>
 </div>
+
 <footer>
   <p>Vous êtes connecté en tant que <span><strong>Véro Fleurs</strong></span></p>
+
 </footer>
+
 </body>
+
 </html>
---
+++ traitementClient.php
@@ -0,0 +1,39 @@
+<?php
+try
+{
+
+    $bdd = new PDO('mysql:host=localhost;dbname=ShareLozere;charset=utf8', 'root', '33forza');
+    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (Exception $e)
+{
+  die('Erreur : ' . $e->getMessage());
+}
+
+
+// $passwordCrypted = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+try
+{
+  $req = $bdd->prepare('INSERT INTO client (photo, ecommerce, marketing, referencement, formation, conseil, design, site, mobile) VALUES(:true, :ecommerce, :marketing, :referencement, :formation, :conseil, :desing, :site, :mobile)');
+  $req->execute(array(
+    'photo' => $_POST['photo'],
+    'ecommerce' => $_POST['true'],
+    'marketing' => $_POST['true'],
+    'referencement'=> $_POST['true'],
+    'formation'=> $_POST['true'],
+    'conseil'=> $_POST['true'],
+    'design'=> $_POST['true'],
+    'site'=> $_POST['true'],
+    'mobile'=> $_POST['true'],
+
+  ));
+
+echo "entré";
+}
+catch (Exception $e)
+{
+  echo "vous êtes inscrit";
+  //die('Erreur : ' . $e->getMessage());
+}
+ ?>
